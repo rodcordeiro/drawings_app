563459cdf157791635e740ca8b8b0e7dd9ed31a8
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-10-07 10:21:50 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { Navigation } from './routes';
 import { ReferenceHook } from './hooks/references';
+import { Toasts } from '@backpackapp-io/react-native-toast';
 
 export default function Drawings() {
   return (
     <ReferenceHook>
-      <Navigation />
+      <Navigation /> <Toasts />
       <StatusBar style="auto" />
     </ReferenceHook>
   );
